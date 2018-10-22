eaa82ea08de6772656f4b4c1595ca265a23edbca
misterSir2006 <ilya.shakhnazarov@gmail.com>
2018-10-22 16:01:11 +0300

    modified sudoku

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,14 +1,11 @@
 
 def read_sudoku(filename):
-    """ Прочитать Судоку из указанного файла """
     digits = [c for c in open(filename).read() if c in '123456789.']
     grid = group(digits, 9)
     return grid
 
 def group(values, n):
     """
-    Сгруппировать значения values в список, состоящий из списков по n элементов
-
     >>> group([1,2,3,4], 2)
     [[1, 2], [3, 4]]
     >>> group([1,2,3,4,5,6,7,8,9], 3)
@@ -16,7 +13,7 @@ def group(values, n):
     """
 
     if len(values) % n != 0:
-        print("Ты даун")
+        print("retard")
         quit()
 
 
