d3308fde84fd312cd8b8faac451127b4df157fe3
misterSir2006 <ilya.shakhnazarov@gmail.com>
2018-10-24 20:30:23 +0300

    added get_row() & get_col()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,22 +1,5 @@
-
-def read_sudoku(filename):
-    digits = [c for c in open(filename).read() if c in '123456789.']
-    grid = group(digits, 9)
-    return grid
-
-
-
-def display(values):
-    """Вывод Судоку """
-    width = 2
-    line = '+'.join(['-' * (width * 3)] * 3)
-    for row in range(9):
-        print(''.join(values[row][col].center(width) + ('|' if str(col) in '25' else '') for col in range(9)))
-        if str(row) in '25':
-            print(line)
-    print()
-
-    
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 def group(values, n):
     """
@@ -27,13 +10,83 @@ def group(values, n):
     """
 
     if len(values) % n != 0:
-        print("retard")
+        print("u = retard")
         quit()
-
 
     length = len(values)
     elemsInPart = len(values) // n
 
     return [ values[i*length // elemsInPart : (i+1)*length // elemsInPart] for i in range(elemsInPart)]
 
-print(group([1,2,3,4,5,6,7,8,9], 3))
+
+
+def read_sudoku(filename):
+    digits = [c for c in open(filename).read() if c in '123456789.']
+    grid = group(digits, 9)
+    return grid
+
+
+
+def display(values):
+    width = 2
+    line = '+'.join(['-' * (width * 3)] * 3)
+    for row in range(9):
+        print(''.join(values[row][col].center(width) + ('|' if str(col) in '25' else '') for col in range(9)))
+        if str(row) in '25':
+            print(line)
+    print()
+
+
+
+def get_row(values, pos):
+    """ Возвращает все значения для номера строки, указанной в pos
+    >>> get_row([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
+    ['1', '2', '.']
+    >>> get_row([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (1, 0))
+    ['4', '.', '6']
+    >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
+    ['.', '8', '9']
+    """
+    gridGovna = read_sudoku("puzzle1.txt")
+    rowNumber, colNumber = pos
+
+    row = gridGovna[rowNumber] 
+    return row
+gridGovna = read_sudoku("puzzle1.txt")
+print (get_row(gridGovna, (0, 0)))
+
+
+
+def get_col(values, pos):
+    """ Возвращает все значения для номера столбца, указанного в pos
+    >>> get_col([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
+    ['1', '4', '7']
+    >>> get_col([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (0, 1))
+    ['2', '.', '8']
+    >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
+    ['3', '6', '9']
+    """
+    gridGovna = read_sudoku("puzzle1.txt")
+    rowNumber, colNumber = pos
+    col = []
+
+    for i in range(9):
+        col.append(gridGovna[i][colNumber])
+    return col
+print(get_col(gridGovna, (0, 5)))
+
+
+
+
+def get_block(values, pos):
+    """ Возвращает все значения из квадрата, в который попадает позиция pos
+    >>> grid = read_sudoku('puzzle1.txt')
+    >>> get_block(grid, (0, 1))
+    ['5', '3', '.', '6', '.', '.', '.', '9', '8']
+    >>> get_block(grid, (4, 7))
+    ['.', '.', '3', '.', '.', '1', '.', '.', '6']
+    >>> get_block(grid, (8, 8))
+    ['2', '8', '.', '.', '.', '5', '.', '7', '9']
+    """
+    pass
+
