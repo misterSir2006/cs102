f2166005683da80457eb1864aa590e57857be583
misterSir2006 <ilya.shakhnazarov@gmail.com>
2018-10-26 11:22:10 +0300

    added find_empty_positions()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -53,7 +53,7 @@ def get_row(values, pos):
     row = gridGovna[rowNumber] 
     return row
 gridGovna = read_sudoku("puzzle1.txt")
-print (get_row(gridGovna, (0, 0)))
+print (get_row(gridGovna, (8, 0)))
 
 
 
@@ -73,7 +73,7 @@ def get_col(values, pos):
     for i in range(9):
         col.append(gridGovna[i][colNumber])
     return col
-print(get_col(gridGovna, (0, 5)))
+print(get_col(gridGovna, (0, 0)))
 
 
 
@@ -88,5 +88,56 @@ def get_block(values, pos):
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+    row, col = pos
+    blockList = []
+    if (row == 0 or row == 1 or row == 2)  and (col == 0 or col == 1 or col == 2):
+        blockList = (gridGovna[0][0:3] + gridGovna[1][0:3] + gridGovna[2][0:3])
+        
+    elif (row == 3 or row == 4 or row == 5) and (col == 0 or col == 1 or col == 2):
+        blockList = (gridGovna[0][3:6] + gridGovna[1][3:6] + gridGovna[2][3:6])
+        
+    elif (row == 6 or row == 7 or row == 8) and (col == 0 or col == 1 or col == 2):
+        blockList = (gridGovna[0][6:9] + gridGovna[1][6:9] + gridGovna[2][6:9])
+        
+    elif (row == 0 or row == 1 or row == 2) and (col == 3 or col == 4 or col == 5):
+        blockList = (gridGovna[3][0:3] + gridGovna[4][0:3] + gridGovna[5][0:3])
+
+    elif (row == 3 or row == 4 or row == 5) and (col == 3 or col == 4 or col == 5):
+        blockList = (gridGovna[3][3:6] + gridGovna[4][3:6] + gridGovna[5][3:6])
+
+    elif (row == 6 or row == 7 or row == 8) and (col == 3 or col == 4 or col == 5):
+        blockList = (gridGovna[3][6:9] + gridGovna[4][6:9] + gridGovna[5][6:9])
+  
+    elif (row == 0 or row == 1 or row == 2) and (col == 6 or col == 7 or col == 8):
+        blockList = (gridGovna[6][0:3] + gridGovna[7][0:3] + gridGovna[8][0:3])
+ 
+    elif (row == 3 or row == 4 or row == 5) and (col == 6 or col == 7 or col == 8):
+        blockList = (gridGovna[6][3:6] + gridGovna[7][3:6] + gridGovna[8][3:6])
+        
+    elif (row == 6 or row == 7 or row == 8) and (col == 6 or col == 7 or col == 8):
+        blockList = (gridGovna[6][6:9] + gridGovna[7][6:9] + gridGovna[8][6:9])
+        
+    return blockList
+
+print(get_block(gridGovna, (8, 6)))
+
+
+def find_empty_positions(grid):
+    """ Найти первую свободную позицию в пазле
+    
+    >>> find_empty_positions([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']])
+    (0, 2)
+    >>> find_empty_positions([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']])
+    (1, 1)
+    >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
+    (2, 0)
+    """
+    for i in range(len(gridGovna)):
+        for j in range(len(gridGovna)):
+             if gridGovna[i][j] == '.':
+                 return i, j
+
+print("\n")
+print(find_empty_positions(gridGovna))
+
 
