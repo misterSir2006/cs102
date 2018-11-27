5fcda8600d534dfb3e12c96d6417261f23e2b868
misterSir2006 <ilya.shakhnazarov@gmail.com>
2018-11-27 11:16:50 +0300

    added class

--- homework03/life_with_classes.py
+++ homework03/life_with_classes.py
@@ -4,6 +4,44 @@ import random
 from copy import deepcopy
 
 
+class Cell:
+
+    def __init__(self, row, col, state=False):
+        pass
+
+    def is_alive(self):
+        pass
+
+
+class CellList:
+
+    def __init__(self, nrows, ncols, randomize=False):
+        pass
+
+    def get_neighbours(self, cell):
+        neighbours = []
+        # PUT YOUR CODE HERE
+        return neighbours
+
+    def update(self):
+        new_clist = deepcopy(self)
+        # PUT YOUR CODE HERE
+        return self
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    @classmethod
+    def from_file(cls, filename):
+        pass
+
+
 class GameOfLife:
 
     def __init__(self, width=640, height=480, cell_size=10, speed=10):
@@ -56,42 +94,4 @@ class GameOfLife:
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
-
-
-class Cell:
-
-    def __init__(self, row, col, state=False):
-        pass
-
-    def is_alive(self):
-        pass
-
-
-class CellList:
-
-    def __init__(self, nrows, ncols, randomize=False):
-        pass
-
-    def get_neighbours(self, cell):
-        neighbours = []
-        # PUT YOUR CODE HERE
-        return neighbours
-
-    def update(self):
-        new_clist = deepcopy(self)
-        # PUT YOUR CODE HERE
-        return self
-
-    def __iter__(self):
-        pass
-
-    def __next__(self):
-        pass
-
-    def __str__(self):
-        pass
-
-    @classmethod
-    def from_file(cls, filename):
-        pass
 
