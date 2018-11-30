5806682d94b63f8bbc93f36fb008f22b8148501e
misterSir2006 <ilya.shakhnazarov@gmail.com>
2018-11-30 10:46:26 +0300

    finished

--- homework04/messages.py
+++ homework04/messages.py
@@ -12,7 +12,7 @@ import config
 
 Dates = List[datetime.date]
 Frequencies = List[int]
-
+user_id = int(input('Введите id пользователя - '))
 
 plotly.tools.set_credentials_file(
     username=config.PLOTLY_CONFIG['username'],
@@ -24,18 +24,33 @@ def fromtimestamp(ts: int) -> datetime.date:
     return datetime.datetime.fromtimestamp(ts).date()
 
 
-def count_dates_from_messages(messages: List[Message]) -> Tuple[Dates, Frequencies]:
+def count_dates_from_messages(messages: dict) -> list:
     """ Получить список дат и их частот
 
     :param messages: список сообщений
     """
-    # PUT YOUR CODE HERE
+    dates = []
+    for message in messages:
+        dates.append(message['date'])
+    for i in range(len(dates)):
+        dates[i] = datetime.datetime.fromtimestamp(dates[i]).strftime("%Y-%m-%d")
+    if dates:
+        return dates
 
 
-def plotly_messages_freq(dates: Dates, freq: Frequencies) -> None:
+
+def plotly_messages_freq(dates: list) -> None:
     """ Построение графика с помощью Plot.ly
 
     :param date: список дат
     :param freq: число сообщений в соответствующую дату
     """
-    # PUT YOUR CODE HERE
+    s = Counter(dates)
+    xCoor = list(s.keys())
+    yCoor = list(s.values())
+    stats = [go.Scatter(x=xCoor, y=yCoor)]
+    py.iplot(stats)
+
+if __name__ == '__main__':
+    s = count_dates_from_messages(messages_get_history(user_id))
+    plotly_messages_freq(s)
